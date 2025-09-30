9864f217146b75c5e14463a7ce0a79674d303c98
ArtemKarDev <akm86@mail.ru>
2025-09-30 23:58:26 +0300

    change hellow

--- src/main/java/ru/karlashov/Main.java
+++ src/main/java/ru/karlashov/Main.java
@@ -1,5 +1,7 @@
-package ru.karlashov;public class Main {
+package ru.karlashov;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello qaguru!");
     }
 }
